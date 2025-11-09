60146d21fcaaad56e95acc16e8d6a15c6fad8257
bibekshah220 <bibekshah425@gmail.com>
2025-11-09 18:57:20 +0545

    update

--- src/server.js
+++ src/server.js
@@ -35,7 +35,12 @@ const middleware2 = (req, res, next) => {
 
 // * using middleware
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173/",
+    credentials: true,
+  })
+);
 app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
 
